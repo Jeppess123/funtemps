303bebfa23c87b40d40a745e9fd9d4ad9188d801
Jeppess123 <jeppestromberg@gmail.com>
2023-02-15 15:24:57 +0100

    comitter i funtemps mappen

--- conv/conv.go
+++ conv/conv.go
@@ -1,22 +1,37 @@
 package conv
 
-/*
-  I denne pakken skal alle konverteringfunksjonene
-  implementeres. Bruk engelsk.
-    FarhenheitToCelsius
-    CelsiusToFahrenheit
-    KelvinToFarhenheit
-    ...
-*/
-
 // Konverterer Farhenheit til Celsius
-func FarhenheitToCelsius(value float64) float64 {
-	// Her skal du implementere funksjonen
-	// Du skal ikke formattere float64 i denne funksjonen
-	// Gjør formattering i main.go med fmt.Printf eller
-	// lignende
-	return 56.67
+func FarhenheitToCelsius(fahr float64) float64 {
+	var cel = (fahr - 32) * 5 / 9
+	return cel
 }
 
-// De andre konverteringsfunksjonene implementere her
-// ...
+// Konverterer Celsius til Farhenheit
+func CelsiusToFahrenheit(cel float64) float64 {
+	var farh = cel*9/5 + 32
+	return farh
+}
+
+// Konverterer Kelvin til Farhenheit
+func KelvinToFarhenheit(kel float64) float64 {
+	var fahr = (kel-273.15)*9/5 + 32
+	return fahr
+}
+
+// Konverterer Farhenheit til Kelvin
+func FahrenheitToKelvin(fahr float64) float64 {
+	var kel = (fahr-32)*5/9 + 273.15
+	return kel
+}
+
+// Konverterer Celsius til Kelvin
+func CelsiusToKelvin(cel float64) float64 {
+	var kel = cel + 273.15
+	return kel
+}
+
+// Konverterer Kelvin til Celsius
+func KelvinToCelsius(kel float64) float64 {
+	var cel = kel - 273.15
+	return cel
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jeppess123/funtemps
+
+go 1.19
